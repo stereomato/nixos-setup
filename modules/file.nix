0e27cbf1a210c949e99fafe0badfaa1ca789fb82
stereomato <stereomato@proton.me>
2024-02-23 14:59:09 -0500

    put stuff under modules/

--- modules/file.nix
+++ modules/file.nix
@@ -1,5 +1,6 @@
-{ config, installPath }: {
-	# FIXME: https://www.reddit.com/r/NixOS/comments/vh2kf7/home_manager_mkoutofstoresymlink_issues/
+{ config, installPath, ...  }: {
+	home = {
+		# FIXME: https://www.reddit.com/r/NixOS/comments/vh2kf7/home_manager_mkoutofstoresymlink_issues/
 		# Symlink configuration files for programs that modify their settings at runtime/exit
 		# https://github.com/nix-community/home-manager/issues/3514
 		# FIXME: Some of these configurations can be handled better, so, do that.
@@ -9,4 +10,6 @@
 		file.".config/neofetch".source = config.lib.file.mkOutOfStoreSymlink "${installPath}/modules/home-manager/to-symlink/neofetch";
 		file.".config/xmrig-mo".source = config.lib.file.mkOutOfStoreSymlink "${installPath}/modules/home-manager/to-symlink/xmrig-mo";
 		file.".config/MangoHud".source = config.lib.file.mkOutOfStoreSymlink "${installPath}/modules/home-manager/to-symlink/MangoHud";
+	};
+
 }
