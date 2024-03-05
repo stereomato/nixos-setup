5245b45c5a39a10ef3f4ec2a1406bd56989ca09e
stereomato <stereomato@proton.me>
2024-03-05 13:36:04 -0500

    modularize configuration a bit

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,64 +1,15 @@
 { pkgs, ... }:
 {
+	imports = [
+		./imports/sysctl-tweaks.nix
+	];
+	
 	boot = {
 		plymouth = {
 			enable = true;
 			font = "${pkgs.inter}/share/fonts/truetype/InterVariable.ttf";
 		};
-		kernel.sysctl = {
-			# Memory
-			# Disable swap read ahead, increases latency when dealing with swap and it's rather meaningless when using zram+zstd anyway
-			"vm.page-cluster" = 0;
-			# Hugepages configuration, mostly for xmrig
-			# Not needed anymore
-			#"vm.nr_hugepages" = 25;
-			#"vm.nr_overcommit_hugepages" = 150;
-			# Prefer to keep filesystem cache memory over application memory
-			"vm.vfs_cache_pressure" = 50;
-			# Proper swappiness
-			"vm.swappiness" = 150;
-			# Set the bytes of my current laptop's storage speed
-			# dirty_bytes is set using  75% of the write speed obtained from the benchmark runs, using the NVME SSD preset
-			# dirty_bytes uses peak performance profile
-			# dirty_background_bytes is set by dividing dirty_bytes by 8
-			# actual value is 1000000000
-			"vm.dirty_bytes" = 500000000;
-			"vm.dirty_background_bytes" = 125000000;
-			# 5% physical ram
-			"vm.min_free_kbytes" = 308229;
-			# Best value, according to phoronix
-			"vm.page_lock_unfairness" = 3;
-			# Disable watermark boosting
-			#"vm.watermark_boost_factor" = 0; # Needed when not using the zen-kernel
-			# Increase kswapd activity
-			# When free memory is less than 2.5%, make kswapd kick in.
-			# https://unix.stackexchange.com/a/679203
-			"vm.watermark_scale_factor" = 150;
-			# Increase the compaction activity slightly
-			#"vm.compaction_proactiveness" = 25;
-			# Compact also unevictable memory (testing)
-			#"vm.compact_unevictable_allowed" = 1;
-			
-			# Fedora change, for some games. Shouldn't affect most things
-			# Higher memory map count for some games that need it
-			# https://www.phoronix.com/news/Fedora-39-Max-Map-Count-Approve
-			"vm.max_map_count" = 1048576;
-
-			# Internet
-			"net.ipv4.tcp_fastopen" = 3;
-			"net.ipv4.tcp_low_latency" = 1;
-			"net.ipv4.tcp_ecn" = 1;
-			"net.ipv4.tcp_congestion_control" = "bbr";
-
-			# Cpu
-			#"kernel.sched_child_runs_first" = 1;
-			#"kernel.sched_cfs_bandwidth_slice_us" = 3000;
-			#"kernel.perf_cpu_time_max_percent" = 3;
-			
-			# Intel GPU
-			# Disable the paranoid mode over some stats
-			"dev.i915.perf_stream_paranoid" = 0;
-		};
+		
 		initrd = {
 			availableKernelModules = [ "i915" "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
 			kernelModules = [ ];
