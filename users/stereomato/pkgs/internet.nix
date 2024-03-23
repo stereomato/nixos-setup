9582e67947893d0ff8ea90af87548d00d8dffa2a
stereomato <stereomato@proton.me>
2024-03-23 11:11:43 -0500

    highlights: explain how I'm still dealing with nixOS on my README.md, some updates, some stuff put in their own dedicated nix files (such as gaming and internet programs). - firefox: move to system-wide stuff (might move it back to user) - fonts.nix: put the font rendering freetype environment variable there. - performance.nix: switch back to zstd. - overlays.nix: remove firefox. - sysctl-tweaks.nix: try vfs_cache_pressure=200 again. - media/consumption.nix: move yt-dlp to here

---
+++ users/stereomato/pkgs/internet.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:{
+  home.packages = with pkgs; [
+    # Web Browsers
+    google-chrome vivaldi vivaldi-ffmpeg-codecs 
+    
+    # Ask for it to be fixed someday
+    #vivaldi-widevine 
+
+    # Chat apps
+    tdesktop discord gnome.polari mumble fractal element-desktop dino
+
+    # Password management
+		bitwarden bitwarden-cli
+
+    # Internet tools
+	  curl wget aria fragments megacmd
+    # VPN
+		protonvpn-gui
+  ];
+
+  services.syncthing = {
+		enable = true;
+	};
+}
