0d093239fe066d6b20d3a91c9e92875e6e216a66
stereomato <stereomato@proton.me>
2025-01-03 21:45:47 -0500

    adapt config for Hearts

--- machines/Hearts/default.nix
+++ machines/Hearts/default.nix
@@ -6,8 +6,8 @@
 		"iwlwifi.power_save=1"
 		"iwlwifi.power_level=3"
 	];
-  boot.initrd.luks.devices."HeartDisk" = {
-		device = "";
+  boot.initrd.luks.devices."HeartsDisk" = {
+		device = "/dev/disk/by-uuid/f40dcced-9c73-42f9-9b5c-b250e9e3bb6f";
 		allowDiscards = true;
 		bypassWorkqueues = true;
 	};
@@ -29,6 +29,55 @@
 		xserver = {
 			# Enable for GNOME
 			desktopManager.gnome.enable = true;
+		};
+	};
+
+	fileSystems = {
+		"/" = {
+			device = "/dev/mapper/HeartsDisk";
+			fsType = "btrfs";
+			options = [
+				"subvol=@"
+				"compress=zstd:1"
+			];
+		};
+		"/boot" = {
+			device = "/dev/disk/by-uuid/5ADB-0DC5";
+			fsType = "vfat";
+			options = [
+				"discard"
+			];
+		};
+		 "/nix" = {
+			device = "/dev/mapper/HeartsDisk";
+			fsType = "btrfs";
+			options = [
+				"compress=zstd:2"
+				"subvol=@nix"
+			];
+		};
+		"/home" = {
+			device = "/dev/mapper/HeartsDisk";
+			fsType = "btrfs";
+			options = [
+				"compress=zstd:2"
+				"subvol=@home"
+			];
+		};
+		"/swap" = {
+			device = "/dev/mapper/HeartsDisk";
+			fsType = "btrfs";
+			options = [
+				"subvol=@swap"
+			];
+		};
+		"/var/log" = {
+			device = "/dev/mapper/HeartsDisk";
+			fsType = "btrfs";
+			options = [
+				"compress=zstd:10"
+				"subvol=@var_log"
+			];
 		};
 	};
 }
